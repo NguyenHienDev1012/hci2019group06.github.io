a60d0cc748da37fb34678b207e26d2351b808a4e
Hiếu Đặng Trung <hieudt230899@gmail.com>
2019-12-17 13:57:08 +0700

    commit code about team member

---
+++ script/gallery.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+    
+  
+
+
+
